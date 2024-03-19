683cd3392d18ce2552a3ff72545a9b228b66bab9
shiftALpha <ranadhirav08@gmail.com>
2024-03-19 12:28:46 +0200

    Test for build

--- lib/widget/footer.dart
+++ lib/widget/footer.dart
@@ -11,7 +11,7 @@ class Footer extends StatelessWidget {
     final _themeProvider = Provider.of<ThemeProvider>(context);
     double height = MediaQuery.of(context).size.height;
     double width = MediaQuery.of(context).size.width;
-    return Container(
+    return Container (
       margin: EdgeInsets.fromLTRB(0, height * 0.05, 0, 0),
       height: height * 0.07,
       width: width,
